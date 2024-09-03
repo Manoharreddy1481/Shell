6731968586a15c1df38b3e01f83696ffcad08308
Manoharreddy1481 <pullagurlamanoharreddy@gmail.com>
2024-09-03 15:31:58 +0530

    Shell

--- 02-functions-install-mysql.sh
+++ 02-functions-install-mysql.sh
@@ -6,21 +6,24 @@
 # 4. check already installed or not, if installed tell the user it is already insalled
 # 5. if not installed, install it
 # 6. check it is success or not
-
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-    echo " proceed to install the mysql with root previllages"
+    echo -e " $R proceed to install the mysql with root previllages $N"
     exit 1
 fi
 
 VALIDATE() {
     if [ $1 -ne 0 ]
     then
-        echo "$2 is not installed"
+        echo " $R $2 is not installed $N"
         exit 1
     else
-        echo "$2 is installed"
+        echo " $G $2 is installed  $N"
     fi
 
 }
@@ -29,24 +32,24 @@ dnf list installed mysql
 
 if [ $? -ne 0 ]
 then
-    echo "Proceed on installation of mysql"
+    echo " $Y Proceed on installation of mysql $N"
     dnf install mysql -y
-    VALIDATE $? "Installing MYSQL" 
+    VALIDATE $? "$Y Installing MYSQL $N" 
 else
-    echo "MYSql is already installed."
+    echo "$G MYSql is already installed. $N"
 fi
 
-echo "Working on installation of GIT"
+echo "$Y Working on installation of GIT $N"
 
 dnf list installed git
 
 if [ $? -ne 0 ]
 then 
-    echo "Proceed to install the GIT"
+    echo " $G Proceed to install the GIT $N"
     dnf install git -y
-    VALIDATE $? "Installing Git"
+    VALIDATE $? " $G Installing Git $N"
 else 
-    echo "GIT is already installed." 
+    echo "$G GIT is already installed. $N" 
 fi
 
 
