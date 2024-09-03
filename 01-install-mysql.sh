86fa0d2b0d8eedc2574074ed64a0d1f432a92270
Manoharreddy1481 <pullagurlamanoharreddy@gmail.com>
2024-09-03 14:31:26 +0530

    Shell

---
+++ 01-install-mysql.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# 1. check the user has root access or not
+# 2. if root access, proceed with the script
+# 3. otherwise through the error
+# 4. check already installed or not, if installed tell the user it is already insalled
+# 5. if not installed, install it
+# 6. check it is success or not
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo " proceed to install the mysql with root previllages"
+    exit 1
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then
+    echo "Proceed on installation of mysql
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "installation of mysql is UNSUCCESS"
+    else 
+        echo "installation of mysql is SUCCESS"
+    fi
+else
+    echo "MYSql is already installed."
+fi
+
+
+
+
+
