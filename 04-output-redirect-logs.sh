8e6ade68978dfbcc924a5ef0648a1ca25ac6a8d1
Manoharreddy1481 <pullagurlamanoharreddy@gmail.com>
2024-09-03 19:08:42 +0530

    Shell

---
+++ 04-output-redirect-logs.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+USERID=$(id -u)
+
+LOGS_FOLDER="var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP
+mkdir -p $LOGS_FOLDER
+
+CHECK_ROOT(){
+
+if [ $USERID -ne 0 ]
+then
+    echo -e " $R proceed to install the mysql with root previllages $N" 
+    exit 1
+fi
+
+}
+
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo -e " $R $2 is not installed $N" &>>LOG_FILE
+        exit 1
+    else
+        echo -e " $G $2 is installed  $N" &>>LOG_FILE
+    fi
+
+}
+
+CHECK_ROOT
+
+echo " script runnig date is: $(date)"
+
+for package in $@
+do 
+    dnf list installed $package
+    if [ $? -ne 0 ]
+    then 
+        echo -e "$G $package is not installed..... Please install the package.. $N" &>>LOG_FILE
+        dnf install $package -y
+        VALIDATE $? "Installing $package"
+    else
+        echo -e "$G $package Already installed $N" &>>LOG_FILE
+    fi
+done
