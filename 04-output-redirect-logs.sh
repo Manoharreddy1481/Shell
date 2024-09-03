0f8a2fe8125b273b662544b7a87abf3560cc5d65
Manoharreddy1481 <pullagurlamanoharreddy@gmail.com>
2024-09-03 19:44:59 +0530

    Shell

--- 04-output-redirect-logs.sh
+++ 04-output-redirect-logs.sh
@@ -17,7 +17,7 @@ CHECK_ROOT(){
 
 if [ $USERID -ne 0 ]
 then
-    echo -e " $R proceed to run this script with root previllages $N" &>>$LOG_FILE
+    echo -e " $R proceed to run this script with root previllages $N" | tee -a $LOG_FILE
     exit 1
 fi
 
@@ -27,10 +27,10 @@ fi
 VALIDATE() {
     if [ $1 -ne 0 ]
     then
-        echo -e "$R $2 is not installed $N" &>>$LOG_FILE
+        echo -e "$R $2 is not installed $N" | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$G $2 is installed  $N" &>>$LOG_FILE
+        echo -e "$G $2 is installed  $N" | tee -a $LOG_FILE
     fi
 
 }
@@ -41,13 +41,13 @@ echo " script runnig date is: $(date)"
 
 for package in $@
 do 
-    dnf list installed $package &>>$LOG_FILE
+    dnf list installed $package | tee -a $LOG_FILE
     if [ $? -ne 0 ]
     then 
-        echo -e "$G $package is not installed..... Please install the package.. $N" &>>$LOG_FILE
+        echo -e "$G $package is not installed..... Please install the package.. $N" | tee -a $LOG_FILE
         dnf install $package -y &>>$LOG_FILE
         VALIDATE $? "Installing $package"
     else
-        echo -e "$G $package Already installed $N" &>>$LOG_FILE
+        echo -e "$G $package Already installed $N" | tee -a $LOG_FILE
     fi
 done
