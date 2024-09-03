428c45cbf63016e2f60edbe7e8986ff577666f5e
Manoharreddy1481 <pullagurlamanoharreddy@gmail.com>
2024-09-03 19:18:06 +0530

    Shell

--- 04-output-redirect-logs.sh
+++ 04-output-redirect-logs.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+LOGS_FOLDER="var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 USERID=$(id -u)
-
-LOGS_FOLDER="var/log/shell-script"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-mkdir -p $LOGS_FOLDER
-LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP
 
 
 CHECK_ROOT(){
