5e1dd7b10d8439c913ff4a018b310f0a1416f030
Manoharreddy1481 <pullagurlamanoharreddy@gmail.com>
2024-09-04 16:36:41 +0530

    Shell

---
+++ 07 check-diskspace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5
+
+while IFS = read -r line
+do
+    USAGE=$(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(df -hT | grep xfs | awk -F " " '{print $NF}' )
+    if [ $USAGE -gt $DISK_THRESHOLD ]
+    then 
+        echo "$PARTITION is more than $DISK_THRESHOLD, Current Value :$USAGE. Please check......"
+    fi
+done <<< $DISK_USAGE
+
