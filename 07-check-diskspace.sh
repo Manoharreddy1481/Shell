2e543951e2d98e8c58504a56c15761ef3a568090
Manoharreddy1481 <pullagurlamanoharreddy@gmail.com>
2024-09-04 16:41:02 +0530

    Shell

--- 07-check-diskspace.sh
+++ 07-check-diskspace.sh
@@ -2,10 +2,10 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5
-while IFS= read -r file
+while IFS= read -r line
 do
-    USAGE=$(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    PARTITION=$(df -hT | grep xfs | awk -F " " '{print $NF}' )
+    USAGE=$( echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$( echo $line | grep xfs | awk -F " " '{print $NF}' )
     if [ $USAGE -gt $DISK_THRESHOLD ]
     then 
         echo "$PARTITION is more than $DISK_THRESHOLD, Current Value :$USAGE. Please check......"
