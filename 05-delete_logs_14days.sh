b227376a6aef6476d2d439f02ef184497c361698
Manoharreddy1481 <pullagurlamanoharreddy@gmail.com>
2024-09-04 14:24:42 +0530

    Shell

--- 05-delete_logs_14days.sh
+++ 05-delete_logs_14days.sh
@@ -16,13 +16,3 @@ do
     rm -rf $file
 done <<< $files
 
-
-# FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
-# echo "Files: $FILES"
-
-# # dont use line, it is reserverd word
-# while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
-# do
-#     echo "Deleting file: $file"
-#     rm -rf $file
-# done <<< $FILES
